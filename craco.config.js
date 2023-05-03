1986d6c268be45a99573d8551dff7cce89646573
Allison Bellows <alibellows@gmail.com>
2023-05-03 15:33:45 -0700

    add mattrglobal lib, fix webpack issues so it builds (fixed for Sonoma)

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
     webpack: {
         configure: (webpackConfig) => {
@@ -8,11 +10,23 @@ module.exports = {
             const wasmExtensionRegExp = /\.wasm$/;
             webpackConfig.module.rules.forEach((rule) => {
                 (rule.oneOf || []).forEach((oneOf) => {
-                    if (oneOf.type === "asset/resource") {
+                    if (oneOf.type === 'asset/resource') {
                         oneOf.exclude.push(wasmExtensionRegExp);
                     }
                 });
             });
+
+            webpackConfig.resolve.fallback = { 
+                crypto: require.resolve('crypto-browserify'),
+                path: require.resolve('path-browserify'),
+                stream: require.resolve('stream-browserify'),
+                util: require.resolve('util/'),
+            };
+            webpackConfig.plugins.push(
+                new webpack.ProvidePlugin({
+                    process: 'process/browser.js',
+                }),
+            );
             return webpackConfig;
         },
     },
--- src/hooks/queries/useVerifyCredential.ts
+++ src/hooks/queries/useVerifyCredential.ts
@@ -1,5 +1,20 @@
 import { useMutation } from "react-query";
 import { CredentialDerivedProof } from "../../models/credential";
+import { verify, purposes } from "jsonld-signatures";
+import { CryptoLD } from "crypto-ld";
+import {
+    Bls12381G2KeyPair,
+    BbsBlsSignature2020,
+    BbsBlsSignatureProof2020,
+    deriveProof,
+  } from "@mattrglobal/jsonld-signatures-bbs";
+import { Ed25519VerificationKey2020 } from '@digitalbazaar/ed25519-verification-key-2020';
+
+
+// Prepare shitty crytpoLD library
+// const cryptoLd = new CryptoLD();
+// Install driver for Ed255192020
+// cryptoLd.use(Ed25519VerificationKey2020);
 
 type VerifyCredentialProps = {
     derivedProof: CredentialDerivedProof;
@@ -9,15 +24,36 @@ const handleVerifyCredential = async (
     derivedProof: VerifyCredentialProps["derivedProof"]
 ): Promise<any> => {
     console.log(JSON.stringify(derivedProof));
-    const response = await fetch(
-        `${process.env.REACT_APP_VERIFY_ENDPOINT}/api/trinsic/verify`,
-        {
-            method: "POST",
-            body: JSON.stringify(derivedProof),
-        }
-    );
+    // TODO: use Mattr's open source verify instead of Trinsic's
+    // mattrglobal/jsonldsignatures
+    // const proof = await new ProofSet(derivedProof.proof);
+    // const key = new Ed25519KeyPair(derivedProof.proof.verificationMethod);
+    // const key = await cryptoLd.generate({ type: 'Ed25519VerificationKey2020' });
+    // const keyPair = await new Bls12381G2KeyPair();
+    const suite = new BbsBlsSignature2020();
+    const response = await verify(derivedProof.proof, {
+        suite,
+        purpose: new purposes.AssertionProofPurpose(),
+    });
+    // const response = await fetch(
+    //     `${process.env.REACT_APP_VERIFY_ENDPOINT}/api/trinsic/verify`,
+    //     {
+    //         method: "POST",
+    //         body: JSON.stringify(derivedProof),
+    //     }
+    // );
+    // const response = await fetch(
+    //     `${process.env.REACT_APP_VERIFY_ENDPOINT}/api/trinsic/verify`,
+    //     {
+    //         method: "POST",
+    //         body: JSON.stringify(derivedProof),
+    //     }
+    // );
+    console.log("@@@@ Response @@@@@");
+    console.log(JSON.stringify(response));
 
     if (!response.ok) {
+        console.log("@@@@ Uh Oh... @@@@@");
         throw Error("Unable to verify credential.");
     }
 
