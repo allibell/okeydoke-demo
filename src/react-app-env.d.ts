d736635f7d62d7af15d715e50bffbba299e1e4f1
Hersh Patel <hersh.patel@trinsic.id>
2023-07-03 16:21:33 -0400

    Adding typings

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="react-scripts" />
+
+// src/typings/jsonld-signatures.d.ts
+declare module "jsonld-signatures";
+declare module "@digitalbazaar/zcapld";
+declare module "jsonld";
+declare module "crypto-ld";
+declare module "@digitalbazaar/ed25519-verification-key-2020";
+declare module "@digitalbazaar/ed25519-signature-2020";
+declare module "jsonld-document-loader";
+declare module "@decentralized-identity/ion-tools";
+
+// src/typings/simplewebauthn.d.ts
+declare module "@simplewebauthn/browser";
